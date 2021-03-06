dad53dcda7354c25fe001735730d7116c4ca402b
Guilherme Müller <gmullernh@gmail.com>
2021-03-06 15:24:43 -0300

    first commit

---
+++ PDFTTS/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Speech.Synthesis;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+
+namespace PDFTTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SpeechSynthesizer speech = new SpeechSynthesizer();
+
+                // Configurações Voz
+                var voices = speech.GetInstalledVoices();
+
+                Console.WriteLine("Vozes instaladas no sistema.");
+                Console.WriteLine("ID\tNome\tIdioma");
+                for (int i = 0; i < voices.Count; i++)
+                {
+                    Console.WriteLine($"{i}\t{voices[i].VoiceInfo.Name}\t({voices[i].VoiceInfo.Culture})");
+                }
+
+                Console.Write("Digite o ID para escolher a voz: ");
+                int.TryParse(Console.ReadLine(), out int voiceId);
+                Console.WriteLine($"Voz selecionada: {voices[voiceId].VoiceInfo.Name}");
+                speech.SelectVoice(voices[voiceId].VoiceInfo.Name);
+
+                Console.WriteLine(new string('-', 20));
+
+                Console.Write("Velocidade (-10 até 10): ");
+                int.TryParse(Console.ReadLine(), out int speed);
+                Console.WriteLine($"Velocidade selecionada: {speed}");
+
+                Console.WriteLine(new string('-', 20));
+
+                Console.Write("Volume (0 até 200): ");
+                int.TryParse(Console.ReadLine(), out int volume);
+                Console.WriteLine($"Volume selecionado: {volume}");
+
+                speech.Rate = speed;
+                speech.Volume = volume;
+
+                Console.WriteLine(new string('-', 20));
+
+                // Configurações PDF
+
+                string path = string.Empty;
+
+                do
+                {
+                    Console.Write("Digite o caminho do arquivo: ");
+                    path = Console.ReadLine();
+                } while (string.IsNullOrEmpty(path) || !File.Exists(path));
+
+                var pdfDocument = new PdfDocument(new PdfReader(path));
+                int totalPages = pdfDocument.GetNumberOfPages();
+                Console.WriteLine($"O PDF tem {totalPages} páginas.");
+
+                Console.WriteLine(new string('-', 20));
+
+                int pageStart;
+                do
+                {
+                    Console.Write("Digite a página inicial: ");
+                    int.TryParse(Console.ReadLine(), out pageStart);
+                } while (pageStart < 1);
+
+                Console.WriteLine(new string('-', 20));
+
+                int pageEnd;
+                do
+                {
+                    Console.Write("Digite a página final: ");
+                    int.TryParse(Console.ReadLine(), out pageEnd);
+                } while (pageEnd > totalPages || pageEnd < pageStart);
+
+                Console.WriteLine(new string('-', 20));
+                Console.WriteLine("Iniciando a leitura.");
+
+                // Itera e lê as páginas.
+                int curPage = pageStart;
+
+                do
+                {
+                    var page = pdfDocument.GetPage(curPage);
+                    if (page == null)
+                    {
+                        Console.WriteLine($"Pulando: {curPage}");
+                        curPage++;
+                        continue;
+                    }
+
+                    string text = ExtractTextFromPdf(page);
+                    Console.WriteLine(new string('-', 20));
+                    Console.WriteLine($"Página: {curPage}");
+                    Console.WriteLine(text);
+                    Console.WriteLine(new string('-', 20));
+
+                    ReadText(text, speech);
+                    curPage++;
+                }
+                while (curPage <= pageEnd);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+
+        }
+
+        public static void ReadText(string text, SpeechSynthesizer speech)
+        {
+            speech.Speak(text);
+        }
+
+        public static string ExtractTextFromPdf(PdfPage page)
+        {
+            var strategy = new LocationTextExtractionStrategy();
+            return PdfTextExtractor.GetTextFromPage(page, strategy).Replace("\n", " ").Replace("  ", " ");
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# PDF to TTS
+
+A simple console application for **Windows** (.NET Framework 4.5 or above) to read PDF content using the Windows TTS Speech Library.
+
+# How to use
+
+- Select a voice installed on your machine. See (how to install new voices)[https://support.microsoft.com/en-us/topic/how-to-download-text-to-speech-languages-for-windows-10-d5a6b612-b3ae-423f-afa5-4f6caf1ec5d3].
+- Select a reading speed.
+- Select a volume.
+- Type the path to your PDF file.
+- Type the starting page for the reading session.
+- Type the ending page for the reading session.
+
+# Packages
+
+- (System.Speech.Synthesis)[https://docs.microsoft.com/pt-br/dotnet/api/system.speech.synthesis.speechsynthesizer?view=netframework-4.8]
+- (iText7)[https://www.nuget.org/packages/itext7/]
